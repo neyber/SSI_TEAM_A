54ccd2794f11982732292d758066df48179f6f1b
wcolina <walker.cbdiv@gmail.com>
2018-06-08 03:16:20 -0400

    Added logic to close connection to DW.

--- src/main/java/com/groupa/ssi/services/accidentReport/AccidentReportService.java
+++ src/main/java/com/groupa/ssi/services/accidentReport/AccidentReportService.java
@@ -23,32 +23,58 @@ public class AccidentReportService {
     @Value("${ssi.datawarehouse.datasource.password}")
     private String password;
 
-    public List<AccidentReport> findAll(Date startDate, Date endDate) throws SQLException, ClassNotFoundException {
+    public List<AccidentReport> findAll(Date startDate, Date endDate) throws ClassNotFoundException {
         List<AccidentReport> results = new ArrayList<AccidentReport>();
 
-        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-        Connection conn = DriverManager.getConnection(url, userName, password);
+        Connection conn = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
 
-        String SPsql = "EXEC proGetAccidentsReport ?,?";
-        DateFormat df = new SimpleDateFormat("yyyy/MM/dd");
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            conn = DriverManager.getConnection(url, userName, password);
 
-        PreparedStatement ps = conn.prepareStatement(SPsql);
-        ps.setEscapeProcessing(true);
-        ps.setQueryTimeout(60000);
-        ps.setString(1, df.format(startDate));
-        ps.setString(2, df.format(endDate));
-        ResultSet rs = ps.executeQuery();
-        while (rs.next()) {
-            AccidentReport accidentReport = new AccidentReport();
-            accidentReport.setFullName(rs.getString("fullName"));
-            accidentReport.setGender(rs.getString("gender"));
-            accidentReport.setRole(rs.getString("role"));
-            accidentReport.setYear(rs.getInt("year"));
-            accidentReport.setMonth(rs.getString("month"));
-            accidentReport.setDay(rs.getInt("day"));
-            accidentReport.setQuantity(rs.getInt("quantity"));
+            String SPsql = "EXEC proGetAccidentsReport ?,?";
+            DateFormat df = new SimpleDateFormat("yyyy/MM/dd");
 
-            results.add(accidentReport);
+            ps = conn.prepareStatement(SPsql);
+            ps.setEscapeProcessing(true);
+            ps.setQueryTimeout(60000);
+            ps.setString(1, df.format(startDate));
+            ps.setString(2, df.format(endDate));
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                AccidentReport accidentReport = new AccidentReport();
+                accidentReport.setFullName(rs.getString("fullName"));
+                accidentReport.setGender(rs.getString("gender"));
+                accidentReport.setRole(rs.getString("role"));
+                accidentReport.setYear(rs.getInt("year"));
+                accidentReport.setMonth(rs.getString("month"));
+                accidentReport.setDay(rs.getInt("day"));
+                accidentReport.setQuantity(rs.getInt("quantity"));
+
+                results.add(accidentReport);
+            }
+        }
+        catch (SQLException error){
+            System.out.println(error.getMessage());
+        }
+        finally {
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException e) { /* ignored */}
+            }
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) { /* ignored */}
+            }
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException e) { /* ignored */}
+            }
         }
 
         return results;
